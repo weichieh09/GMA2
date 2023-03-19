28a89f41ce9e1616f7f48c063cbab58a099c6119
weichieh09 <weichieh09@gmail.com>
2023-03-19 17:02:49 +0800

    [add] F wcc201 製造商

--- src/main/webapp/app/customized/wcc201/index.ts
+++ src/main/webapp/app/customized/wcc201/index.ts
@@ -1,12 +1,70 @@
 export default {
   data() {
     return {
-      title: 'title',
+      form: {
+        country: null,
+        cerfStatus: null,
+        cerfNo: '',
+      },
+      countryList: [
+        { text: '全選', value: null },
+        { text: '中國', value: 'CN' },
+        { text: '台灣', value: 'TW' },
+      ],
+      cerfStatusList: [
+        { text: '全選', value: null },
+        { text: '有效', value: 'isWork' },
+        { text: '失效', value: 'notWork' },
+        { text: '維護中', value: 'fixing' },
+      ],
+      show: true,
+      cerfs: [
+        {
+          id: '1',
+          cerfNo: 'CI435060200161',
+          areaCd: 'TW',
+          issuFt: '2016-06-13T16:00:00Z',
+          cerfExpDt: '2019-06-12T16:00:00Z',
+          effFg: 'N',
+          applId: '22099738',
+        },
+        {
+          id: '2',
+          cerfNo: 'CI435060208585',
+          areaCd: 'TW',
+          issuFt: '2020-04-12T16:00:00Z',
+          cerfExpDt: '2023-04-12T16:00:00Z',
+          effFg: 'Y',
+          applId: '22099738',
+        },
+        {
+          id: '3',
+          cerfNo: 'Q123',
+          areaCd: 'CN',
+          issuFt: '2020-01-01T16:00:00Z',
+          cerfExpDt: '2023-01-01T16:00:00Z',
+          effFg: 'Y',
+          applId: 'A123',
+        },
+      ],
     };
   },
   methods: {
-    test1(event) {
-      alert('test1');
+    onSubmit(event) {
+      event.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    onReset(event) {
+      event.preventDefault();
+      // Reset our form values
+      this.form.country = null;
+      this.form.cerfStatus = null;
+      this.form.cerfNo = '';
+      // Trick to reset/clear native browser form validation state
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
     },
   },
 };
--- src/main/webapp/app/customized/wcc201/index.vue
+++ src/main/webapp/app/customized/wcc201/index.vue
@@ -2,38 +2,136 @@
   <b-container fluid>
     <b-row>
       <h2 id="page-heading">
-        <span>Wcc201</span>
+        <span v-text="$t('gma2App.wcc201.home.title')">wcc201</span>
       </h2> </b-row
     ><br />
-    <b-row>
-      <b-col cols="6">
-        <select class="form-control">
-          <option v-bind:value="0">國家</option>
-          <!-- <option v-bind:value="itemOption.id" v-for="itemOption in items" :key="itemOption.id">
-              {{ itemOption.name }}
-            </option> -->
-        </select>
-      </b-col>
-      <b-col cols="6">
-        <select class="form-control">
-          <option v-bind:value="0">證書狀態</option>
-          <!-- <option v-bind:value="itemOption.id" v-for="itemOption in items" :key="itemOption.id">
-              {{ itemOption.name }}
-            </option> -->
-        </select>
-      </b-col> </b-row
-    ><br />
+
     <b-row>
       <b-col cols="12">
-        <input type="text" class="form-control" v-bind:placeholder="$t('gmaApp.wcc201.form.cerfNo')" />
+        <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+          <b-row>
+            <b-col cols="6">
+              <b-form-group id="input-group-1" :label="$t('gma2App.wcc201.form1.country')" label-for="input-1">
+                <b-form-select id="input-1" v-model="form.country" :options="countryList"></b-form-select>
+              </b-form-group>
+            </b-col>
+            <b-col cols="6">
+              <b-form-group id="input-group-2" :label="$t('gma2App.wcc201.form1.cerfStatus')" label-for="input-2">
+                <b-form-select id="input-2" v-model="form.cerfStatus" :options="cerfStatusList"></b-form-select>
+              </b-form-group>
+            </b-col>
+          </b-row>
+
+          <b-row>
+            <b-col cols="12">
+              <b-form-group id="input-group-3" :label="$t('gma2App.wcc201.form1.mnfctrUniNo')" label-for="input-3">
+                <b-form-input
+                  id="input-3"
+                  v-model="form.cerfNo"
+                  type="text"
+                  :placeholder="$t('gma2App.wcc201.form1.mnfctrUniNo')"
+                ></b-form-input>
+              </b-form-group>
+            </b-col>
+          </b-row>
+
+          <b-row>
+            <b-col cols="12" class="d-flex justify-content-end">
+              <b-button-group>
+                <b-button type="submit" variant="primary"
+                  ><b-icon icon="search"></b-icon> <span v-text="$t('gma2App.wcc201.home.submitLabel')">search</span></b-button
+                >
+                <b-button type="reset" variant="outline-secondary"
+                  ><b-icon icon="eraser"></b-icon> <span v-text="$t('gma2App.wcc201.home.resetLabel')">eraser</span></b-button
+                >
+              </b-button-group>
+            </b-col>
+          </b-row>
+        </b-form>
       </b-col> </b-row
     ><br />
+
     <b-row>
-      <b-col cols="12" class="d-flex justify-content-end">
-        <button class="btn btn-info mr-2">
-          <font-awesome-icon icon="search"></font-awesome-icon>
-          <span>搜尋</span>
-        </button>
+      <b-col cols="12">
+        <div class="alert alert-warning" v-if="false">
+          <span v-text="$t('gma2App.wcc201.home.notFound')">No data found</span>
+        </div>
+      </b-col>
+      <b-col cols="12">
+        <table class="table table-striped" aria-describedby="cerfs">
+          <thead>
+            <tr>
+              <th scope="row" v-on:click="changeOrder('areaCd')">
+                <span v-text="$t('gma2App.wcc201.table1.areaCd')">Area Cd</span>
+              </th>
+              <th scope="row" v-on:click="changeOrder('cerfNo')">
+                <span v-text="$t('gma2App.wcc201.table1.cerfNo')">Cerf No</span>
+              </th>
+              <th scope="row" v-on:click="changeOrder('cerfVer')">
+                <span v-text="$t('gma2App.wcc201.table1.cerfVer')">Cerf Ver</span>
+              </th>
+              <th scope="row" v-on:click="changeOrder('cerfFee')">
+                <span v-text="$t('gma2App.wcc201.table1.cerfFee')">Cerf Fee</span>
+              </th>
+              <th scope="row" v-on:click="changeOrder('stsCd')">
+                <span v-text="$t('gma2App.wcc201.table1.stsCd')">Sts Cd</span>
+              </th>
+              <th scope="row" v-on:click="changeOrder('applId')">
+                <span v-text="$t('gma2App.wcc201.table1.applId')">Appl Id</span>
+              </th>
+              <th scope="row" v-on:click="changeOrder('lstMtnDt')">
+                <span v-text="$t('gma2App.wcc201.table1.lstMtnDt')">Lst Mtn Dt</span>
+              </th>
+              <th scope="row"></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="cerf in cerfs" :key="cerf.id" data-cy="entityTable">
+              <td>{{ cerf.areaCd }}</td>
+              <td>{{ cerf.cerfNo }}</td>
+              <td>{{ cerf.cerfVer }}</td>
+              <td>{{ cerf.cerfFee }}</td>
+              <td>{{ cerf.stsCd }}</td>
+              <td>{{ cerf.applId }}</td>
+              <td>{{ cerf.lstMtnDt }}</td>
+              <td class="text-right">
+                <b-button variant="success" size="sm"><b-icon icon="eye"></b-icon> <span>詳情Q</span></b-button>
+                <!-- <div class="btn-group">
+                <router-link :to="{ name: 'CerfView', params: { cerfId: cerf.id } }" custom v-slot="{ navigate }">
+                  <button @click="navigate" class="btn btn-info btn-sm details" data-cy="entityDetailsButton">
+                    <font-awesome-icon icon="eye"></font-awesome-icon>
+                    <span class="d-none d-md-inline" v-text="$t('entity.action.view')">View</span>
+                  </button>
+                </router-link>
+                <router-link :to="{ name: 'CerfEdit', params: { cerfId: cerf.id } }" custom v-slot="{ navigate }">
+                  <button @click="navigate" class="btn btn-primary btn-sm edit" data-cy="entityEditButton">
+                    <font-awesome-icon icon="pencil-alt"></font-awesome-icon>
+                    <span class="d-none d-md-inline" v-text="$t('entity.action.edit')">Edit</span>
+                  </button>
+                </router-link>
+                <b-button
+                  v-on:click="prepareRemove(cerf)"
+                  variant="danger"
+                  class="btn btn-sm"
+                  data-cy="entityDeleteButton"
+                  v-b-modal.removeEntity
+                >
+                  <font-awesome-icon icon="times"></font-awesome-icon>
+                  <span class="d-none d-md-inline" v-text="$t('entity.action.delete')">Delete</span>
+                </b-button>
+              </div> -->
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </b-col> </b-row
+    ><br />
+
+    <b-row>
+      <b-col cols="12">
+        <b-card class="mt-3" header="*表單結果" v-if="false">
+          <pre class="m-0">{{ form }}</pre>
+        </b-card>
       </b-col> </b-row
     ><br />
   </b-container>
