d7f3ab67b4d21c0e996e8762a1b6dcc5642d5850
weichieh09 <38759580+weichieh09@users.noreply.github.com>
2023-03-22 00:34:28 +0800

    Update index.vue

--- src/main/webapp/app/customized/wcc111/index.vue
+++ src/main/webapp/app/customized/wcc111/index.vue
@@ -1,176 +1,102 @@
 <template>
   <div class="container">
     <main>
-      <div class="py-5 text-center">
+      <!-- <div class="py-5 text-center">
         <img class="d-block mx-auto mb-4" alt="" width="72" height="57" />
         <h2>Checkout form</h2>
         <p class="lead">
           Below is an example form built entirely with Bootstrap’s form controls. Each required form group has a validation state that can
           be triggered by attempting to submit the form without completing it.
         </p>
+      </div> -->
+
+      <div class="py-3">
+        <h2>詳細內容</h2>
       </div>
 
       <div class="row g-5">
-        <div class="col-md-5 col-lg-4 order-md-last">
-          <h4 class="d-flex justify-content-between align-items-center mb-3">
-            <span class="text-primary">Your cart</span>
-            <span class="badge bg-primary rounded-pill">3</span>
-          </h4>
-          <ul class="list-group mb-3">
-            <li class="list-group-item d-flex justify-content-between lh-sm">
-              <div>
-                <h6 class="my-0">Product name</h6>
-                <small class="text-muted">Brief description</small>
-              </div>
-              <span class="text-muted">$12</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between lh-sm">
-              <div>
-                <h6 class="my-0">Second product</h6>
-                <small class="text-muted">Brief description</small>
-              </div>
-              <span class="text-muted">$8</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between lh-sm">
-              <div>
-                <h6 class="my-0">Third item</h6>
-                <small class="text-muted">Brief description</small>
-              </div>
-              <span class="text-muted">$5</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between bg-light">
-              <div class="text-success">
-                <h6 class="my-0">Promo code</h6>
-                <small>EXAMPLECODE</small>
-              </div>
-              <span class="text-success">−$5</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between">
-              <span>Total (USD)</span>
-              <strong>$20</strong>
-            </li>
-          </ul>
-
-          <form class="card p-2">
-            <div class="input-group">
-              <input type="text" class="form-control" placeholder="Promo code" />
-              <button type="submit" class="btn btn-secondary">Redeem</button>
-            </div>
-          </form>
-        </div>
         <div class="col-md-7 col-lg-8">
-          <h4 class="mb-3">Billing address</h4>
-          <form class="needs-validation" novalidate>
+          <h4 class="mb-3">證書</h4>
+          <form class="needs-validation">
             <div class="row g-3">
               <div class="col-sm-6">
-                <label for="firstName" class="form-label">First name</label>
-                <input type="text" class="form-control" id="firstName" placeholder="" value="" required />
-                <div class="invalid-feedback">Valid first name is required.</div>
+                <label class="form-label">證書編號</label>
+                <input type="text" class="form-control" v-model="cerf.cerfNo" disabled />
+                <br />
               </div>
 
               <div class="col-sm-6">
-                <label for="lastName" class="form-label">Last name</label>
-                <input type="text" class="form-control" id="lastName" placeholder="" value="" required />
-                <div class="invalid-feedback">Valid last name is required.</div>
+                <label class="form-label">證書版本號碼</label>
+                <input type="text" class="form-control" v-model="cerf.cerfVer" disabled />
+                <br />
+              </div>
+
+              <div class="col-sm-6">
+                <label class="form-label">國家/地區代碼</label>
+                <input type="text" class="form-control" v-model="cerf.areaCd" disabled />
+                <br />
+              </div>
+
+              <div class="col-sm-6">
+                <label class="form-label">狀態代碼</label>
+                <input type="text" class="form-control" v-model="cerf.stsCd" disabled />
+                <br />
+              </div>
+
+              <div class="col-sm-6">
+                <label class="form-label">證書申請人ID</label>
+                <input type="text" class="form-control" v-model="cerf.applId" disabled />
+                <br />
+              </div>
+
+              <div class="col-sm-6">
+                <label class="form-label">證書標籤</label>
+                <input type="text" class="form-control" v-model="cerf.cerfTag" disabled />
+                <br />
+              </div>
+
+              <div class="col-sm-6">
+                <label class="form-label">證書發行日期</label>
+                <input type="text" class="form-control" v-model="cerf.issuDt" disabled />
+                <br />
+              </div>
+
+              <div class="col-sm-6">
+                <label class="form-label">證書到期日期</label>
+                <input type="text" class="form-control" v-model="cerf.cerfExpDt" disabled />
+                <br />
               </div>
 
               <div class="col-12">
-                <label for="username" class="form-label">Username</label>
-                <div class="input-group has-validation">
-                  <span class="input-group-text">@</span>
-                  <input type="text" class="form-control" id="username" placeholder="Username" required />
-                  <div class="invalid-feedback">Your username is required.</div>
-                </div>
-              </div>
-
-              <div class="col-12">
-                <label for="email" class="form-label">Email <span class="text-muted">(Optional)</span></label>
-                <input type="email" class="form-control" id="email" placeholder="you@example.com" />
-                <div class="invalid-feedback">Please enter a valid email address for shipping updates.</div>
-              </div>
-
-              <div class="col-12">
-                <label for="address" class="form-label">Address</label>
-                <input type="text" class="form-control" id="address" placeholder="1234 Main St" required />
-                <div class="invalid-feedback">Please enter your shipping address.</div>
-              </div>
-
-              <div class="col-12">
-                <label for="address2" class="form-label">Address 2 <span class="text-muted">(Optional)</span></label>
-                <input type="text" class="form-control" id="address2" placeholder="Apartment or suite" />
-              </div>
-
-              <div class="col-md-5">
-                <label for="country" class="form-label">Country</label>
-                <select class="form-select" id="country" required>
-                  <option value="">Choose...</option>
-                  <option>United States</option>
-                </select>
-                <div class="invalid-feedback">Please select a valid country.</div>
-              </div>
-
-              <div class="col-md-4">
-                <label for="state" class="form-label">State</label>
-                <select class="form-select" id="state" required>
-                  <option value="">Choose...</option>
-                  <option>California</option>
-                </select>
-                <div class="invalid-feedback">Please provide a valid state.</div>
-              </div>
-
-              <div class="col-md-3">
-                <label for="zip" class="form-label">Zip</label>
-                <input type="text" class="form-control" id="zip" placeholder="" required />
-                <div class="invalid-feedback">Zip code required.</div>
+                <label for="email" class="form-label">證書檔案</span></label>
+                <input type="file" class="form-control" />
               </div>
             </div>
 
             <hr class="my-4" />
 
-            <div class="form-check">
-              <input type="checkbox" class="form-check-input" id="same-address" />
-              <label class="form-check-label" for="same-address">Shipping address is the same as my billing address</label>
-            </div>
+            <h4 class="mb-3">廠商</h4>
 
-            <div class="form-check">
-              <input type="checkbox" class="form-check-input" id="save-info" />
-              <label class="form-check-label" for="save-info">Save this information for next time</label>
-            </div>
-
-            <hr class="my-4" />
-
-            <h4 class="mb-3">Payment</h4>
-
-            <div class="my-3">
-              <div class="form-check">
-                <input id="credit" name="paymentMethod" type="radio" class="form-check-input" checked required />
-                <label class="form-check-label" for="credit">Credit card</label>
-              </div>
-              <div class="form-check">
-                <input id="debit" name="paymentMethod" type="radio" class="form-check-input" required />
-                <label class="form-check-label" for="debit">Debit card</label>
-              </div>
-              <div class="form-check">
-                <input id="paypal" name="paymentMethod" type="radio" class="form-check-input" required />
-                <label class="form-check-label" for="paypal">PayPal</label>
-              </div>
-            </div>
-
-            <div class="row gy-3">
-              <div class="col-md-6">
-                <label for="cc-name" class="form-label">Name on card</label>
-                <input type="text" class="form-control" id="cc-name" placeholder="" required />
-                <small class="text-muted">Full name as displayed on card</small>
-                <div class="invalid-feedback">Name on card is required</div>
+            <div v-for="mnfctr in cerf2Mnfctr" :key="mnfctr.id" class="row gy-3">
+              <div class="col-md-12">
+                <label for="cc-name" class="form-label">統一編號</label>
+                <input type="text" class="form-control" disabled />
+                <br />
               </div>
 
               <div class="col-md-6">
-                <label for="cc-number" class="form-label">Credit card number</label>
-                <input type="text" class="form-control" id="cc-number" placeholder="" required />
-                <div class="invalid-feedback">Credit card number is required</div>
+                <label for="cc-name" class="form-label">廠商英文名稱</label>
+                <input type="text" class="form-control" disabled />
+                <br />
               </div>
 
-              <div class="col-md-3">
+              <div class="col-md-6">
+                <label for="cc-number" class="form-label">廠商中文名稱</label>
+                <input type="text" class="form-control" :value="mnfctr.mnfctrNmCh" disabled />
+                <br />
+              </div>
+
+              <!-- <div class="col-md-3">
                 <label for="cc-expiration" class="form-label">Expiration</label>
                 <input type="text" class="form-control" id="cc-expiration" placeholder="" required />
                 <div class="invalid-feedback">Expiration date required</div>
@@ -180,13 +106,59 @@
                 <label for="cc-cvv" class="form-label">CVV</label>
                 <input type="text" class="form-control" id="cc-cvv" placeholder="" required />
                 <div class="invalid-feedback">Security code required</div>
-              </div>
+              </div> -->
             </div>
-
-            <hr class="my-4" />
-
-            <button class="w-100 btn btn-primary btn-lg" type="submit">Continue to checkout</button>
+            <button class="btn btn-info" type="submit" @click="previousState"><b-icon icon="arrow-left"></b-icon> <span>返回</span></button>
           </form>
+        </div>
+        <div class="col-md-5 col-lg-4 order-md-last">
+          <h4 class="d-flex justify-content-between align-items-center mb-3">
+            <span class="text-primary">產品</span>
+            <span v-if="cerf2Prod && cerf2Prod.length > 0" class="badge bg-primary rounded-pill">{{ cerf2Prod.length }}</span>
+          </h4>
+          <ul class="list-group mb-3">
+            <li v-for="prod in cerf2Prod" :key="prod.id" class="list-group-item d-flex justify-content-between lh-sm">
+              <div>
+                <h6 class="my-0">{{ prod.prodNmCh }}</h6>
+                <small class="text-muted">{{ prod.prodId }}</small>
+              </div>
+            </li>
+          </ul>
+          <br />
+          <h4 class="d-flex justify-content-between align-items-center mb-3">
+            <span class="text-primary">費用</span>
+            <span v-if="cerf2CerfFee && cerf2CerfFee.length > 0" class="badge bg-primary rounded-pill">{{cerf2CerfFee.length}}</span>
+          </h4>
+          <ul class="list-group mb-3">
+            <li v-for="cerfFee in cerf2CerfFee" :key="cerfFee.id" class="list-group-item d-flex justify-content-between lh-sm">
+              <div>
+                <h6 class="my-0">{{cerfFee.feeDesc}}</h6>
+                <small class="text-muted">{{cerfFee.feeDt}}</small>
+              </div>
+              <span class="text-muted">${{cerfFee.fee}}</span>
+            </li>
+            <!-- <li class="list-group-item d-flex justify-content-between">
+              <span>Total (USD)</span>
+              <strong>$20</strong>
+            </li> -->
+          </ul>
+          <br />
+          <h4 class="d-flex justify-content-between align-items-center mb-3">
+            <span class="text-primary">檢驗標準</span>
+            <span v-if="cerf2Std && cerf2Std.length > 0" class="badge bg-primary rounded-pill">{{ cerf2Std.length }}</span>
+          </h4>
+          <ul class="list-group mb-3">
+            <li v-for="std in cerf2Std" :key="std.id" class="list-group-item d-flex justify-content-between lh-sm">
+              <div>
+                <h6 class="my-0">{{ std.stdNo }}</h6>
+                <small class="text-muted">{{ std.stdVer }}</small>
+              </div>
+            </li>
+            <!-- <li class="list-group-item d-flex justify-content-between">
+              <span>Total (USD)</span>
+              <strong>$20</strong>
+            </li> -->
+          </ul>
         </div>
       </div>
     </main>
