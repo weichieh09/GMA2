26934c2a4a9c4742f881d861de80551512c98a14
cht_weichieh09 <weichieh09@cht.com.tw>
2023-03-30 17:49:41 +0800

    Revert "[import] JDL gma2_table"
    This reverts commit f2f690c81d55f3e63afdf0bdcfa23956b76fcced.

--- src/main/webapp/app/entities/entities-menu.vue
+++ src/main/webapp/app/entities/entities-menu.vue
@@ -1,41 +1,5 @@
 <template>
   <div>
-    <b-dropdown-item to="/prod">
-      <font-awesome-icon icon="asterisk" />
-      <span v-text="$t('global.menu.entities.prod')">Prod</span>
-    </b-dropdown-item>
-    <b-dropdown-item to="/country">
-      <font-awesome-icon icon="asterisk" />
-      <span v-text="$t('global.menu.entities.country')">Country</span>
-    </b-dropdown-item>
-    <b-dropdown-item to="/cerf">
-      <font-awesome-icon icon="asterisk" />
-      <span v-text="$t('global.menu.entities.cerf')">Cerf</span>
-    </b-dropdown-item>
-    <b-dropdown-item to="/std">
-      <font-awesome-icon icon="asterisk" />
-      <span v-text="$t('global.menu.entities.std')">Std</span>
-    </b-dropdown-item>
-    <b-dropdown-item to="/mark">
-      <font-awesome-icon icon="asterisk" />
-      <span v-text="$t('global.menu.entities.mark')">Mark</span>
-    </b-dropdown-item>
-    <b-dropdown-item to="/company">
-      <font-awesome-icon icon="asterisk" />
-      <span v-text="$t('global.menu.entities.company')">Company</span>
-    </b-dropdown-item>
-    <b-dropdown-item to="/fee">
-      <font-awesome-icon icon="asterisk" />
-      <span v-text="$t('global.menu.entities.fee')">Fee</span>
-    </b-dropdown-item>
-    <b-dropdown-item to="/cerf-company">
-      <font-awesome-icon icon="asterisk" />
-      <span v-text="$t('global.menu.entities.cerfCompany')">Cerf Company</span>
-    </b-dropdown-item>
-    <b-dropdown-item to="/sticker">
-      <font-awesome-icon icon="asterisk" />
-      <span v-text="$t('global.menu.entities.sticker')">Sticker</span>
-    </b-dropdown-item>
     <!-- jhipster-needle-add-entity-to-menu - JHipster will add entities to the menu here -->
   </div>
 </template>
--- src/main/webapp/app/entities/entities.component.ts
+++ src/main/webapp/app/entities/entities.component.ts
@@ -1,28 +1,10 @@
 import { Component, Provide, Vue } from 'vue-property-decorator';
 
 import UserService from '@/entities/user/user.service';
-import ProdService from './prod/prod.service';
-import CountryService from './country/country.service';
-import CerfService from './cerf/cerf.service';
-import StdService from './std/std.service';
-import MarkService from './mark/mark.service';
-import CompanyService from './company/company.service';
-import FeeService from './fee/fee.service';
-import CerfCompanyService from './cerf-company/cerf-company.service';
-import StickerService from './sticker/sticker.service';
 // jhipster-needle-add-entity-service-to-entities-component-import - JHipster will import entities services here
 
 @Component
 export default class Entities extends Vue {
   @Provide('userService') private userService = () => new UserService();
-  @Provide('prodService') private prodService = () => new ProdService();
-  @Provide('countryService') private countryService = () => new CountryService();
-  @Provide('cerfService') private cerfService = () => new CerfService();
-  @Provide('stdService') private stdService = () => new StdService();
-  @Provide('markService') private markService = () => new MarkService();
-  @Provide('companyService') private companyService = () => new CompanyService();
-  @Provide('feeService') private feeService = () => new FeeService();
-  @Provide('cerfCompanyService') private cerfCompanyService = () => new CerfCompanyService();
-  @Provide('stickerService') private stickerService = () => new StickerService();
   // jhipster-needle-add-entity-service-to-entities-component - JHipster will import entities services here
 }
